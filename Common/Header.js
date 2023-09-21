00b12d346af39b36b929fc66216767029dbf95fd
Guru <guruforgithub@gmail.com>
2023-09-21 20:05:03 +0530

    React Common Share Commit

---
+++ Common/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header(props) {
+    return (
+
+        <div id="heading" className="headingClass">
+            <h1>React JS Training</h1>
+            <u><h3>UseState Hook Example 1</h3></u>
+        </div>
+
+    );
+}
+
+export default Header;
